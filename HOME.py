86547eb57d7c4a894dbd6e17a91a650f3e536afd
hrishi331 <hrishikesh331@gmail.com>
2025-08-15 00:41:51 +0530

    added resistance and support finders

--- HOME.py
+++ HOME.py
@@ -14,11 +14,36 @@ weak,moderate, strong and powerfull.
 """
 st.write(text1)
 
-col1,col2 = st.columns(spec=2)
 
-if col1.button(label='INTRADAY >>'):
-    st.switch_page('pages/1_INTRADAY.py')
-if col2.button(label='POSITIONAL >>'):
-    st.switch_page('pages/2_POSITIONAL.py')
+# LEVEL Finders
+with st.expander(label="LEVEL FINDER"):
+
+    col1,col2 = st.columns(spec=2)
+
+    if col1.button(label='INTRADAY LEVELS >>'):
+        st.switch_page('pages/1_INTRADAY_LEVELS.py')
+    if col2.button(label='POSITIONAL LEVELS >>'):
+        st.switch_page('pages/2_POSITIONAL_LEVELS.py')
+
+
+# Resistance Finders
+with st.expander(label="RESISTANCE FINDER"):
+
+    col1,col2 = st.columns(spec=2)
+
+    if col1.button(label='INTRADAY RESISTANCES >>'):
+        st.switch_page('pages/4_INTRADAY_RESISTANCES.py')
+    if col2.button(label='POSITIONAL RESISTANCES >>'):
+        st.switch_page('pages/5_POSITIONAL_RESISTANCES.py')
+
+# Support Finders
+with st.expander(label="SUPPORTS FINDER"):
+
+    col1,col2 = st.columns(spec=2)
+
+    if col1.button(label='INTRADAY SUPPORTS >>'):
+        st.switch_page('pages/6_INTRADAY_SUPPORTS.py')
+    if col2.button(label='POSITIONAL SUPPORTS >>'):
+        st.switch_page('pages/7_POSITIONAL_SUPPORTS.py')
 
 
---
+++ pages/6_INTRADAY_SUPPORTS.py
@@ -0,0 +1,71 @@
+# import packages
+import yfinance as yf
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import streamlit as st 
+import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Header
+st.header("% Change after 'n' days".upper(),divider='gray')
+
+# Take inputs
+    # Select script
+st.subheader("Select script")
+symbol = st.radio(label='Indices',options=['NIFTY 50','NIFTY BANK'],
+         horizontal=True)
+
+symbol_dict = {'NIFTY 50':'^NSEI','NIFTY BANK':'^NSEBANK'}
+
+script = symbol_dict[symbol]
+
+# Select date window 
+st.subheader("Select data window")
+col1,col2 = st.columns(spec=2)
+start = col1.date_input("From : ")
+end = col2.date_input("To : ")
+
+st.info("*Data window should be <= 90 calender days")
+
+
+if st.button(label='SUBMIT'):
+    # Load data 
+
+    df = yf.download(tickers=script,
+                        start=start.strftime("%Y-%m-%d"),
+                        end=end.strftime("%Y-%m-%d"),
+                        interval='60m',
+                        ignore_tz=True)
+    trading_days = df.shape[0]
+    calender_days = end - start
+    st.info(f"Calender days selected : {calender_days.days}")
+    st.info(f"Bars selected : {trading_days}")
+
+    df.columns = ['CLOSE','HIGH','LOW','OPEN','Volume']
+    df.drop('Volume',axis=1,inplace=True)
+
+    # Fix column data type
+    for i in df:
+        df[i] = df[i].astype(float)
+
+    data = round(df['LOW']/50)*50
+    resistance_table = data.value_counts().reset_index().sort_values(by='count')
+    today_price = df['CLOSE'].iloc[-1]
+
+    resistance_table = resistance_table[resistance_table['LOW']<today_price]
+    l1 = np.percentile(resistance_table['count'],33.33)
+    l2 = np.percentile(resistance_table['count'],66.66)
+
+    def classify(i,l1,l2):
+        if i<=l1:
+            return 'WEAK'
+        elif i>l1 and i<=l2:
+            return "MODERATE"
+        else:
+            return 'STRONG'
+        
+    resistance_table['CLASS'] = resistance_table['count'].apply(lambda i : classify(i,l1,l2))
+
+    st.write(resistance_table.set_index('LOW'))
